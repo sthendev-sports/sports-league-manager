9929c90dbc2d079b634a9d520462b460d9f7de5b
Steven Then <sthen1014@gmail.com>
2025-12-28 17:54:01 -0500

    Configure CORS for production

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,16 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Middleware
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local Vite frontend
+      process.env.FRONTEND_URL // Vercel frontend
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 // Import routes
